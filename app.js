f38580bb3a465332ef3ff35bcf54b6208f53cb0f
janar <janar.kallson@gmail.com>
2021-03-26 10:53:09 +0200

    Initial graphql setup

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const { graphqlHTTP } = require("express-graphql")
+const { buildSchema } = require("graphql")
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+        `),
+        rootValue: {
+            events: () => {
+                return ['Test', 'asd', 'qwe'];
+            },
+            createEvent: (args) => {
+                const eventName = args.name;
+                return eventName;
+            }
+        },
+        graphiql: true,
+    }),
+);
+
+app.listen(3000);
