030202113ab2dcfd2245bb9d9e07c0a1eb2afdc9
janar <janar.kallson@gmail.com>
2021-03-29 22:28:13 +0300

    Fixed auth

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
         req.isAuth = false
         return next();
     }
-    const token = authHeader.split(' ')[1]
+    const token = authHeader
     if (!token || '') {
         req.isAuth = false
         return next();
