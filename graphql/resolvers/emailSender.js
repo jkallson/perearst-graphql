3f0aba7aa7ca71d24946a5ad81e6247d258fabf2
janar <janar.kallson@gmail.com>
2021-04-19 16:14:38 +0300

    Added OAuth2 to email sending

--- graphql/resolvers/emailSender.js
+++ graphql/resolvers/emailSender.js
@@ -1,32 +1,39 @@
 const nodemailer = require('nodemailer')
+const { google } = require('googleapis')
 
 module.exports = {
-    sendEmail: (args, req) => {
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.EMAIL_PASSWORD
+    sendEmail: async (args, req) => {
+        const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URL)
+        oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN})
+
+        try {
+            const accessToken = await oAuth2Client.getAccessToken()
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    type: 'OAuth2',
+                    user: process.env.EMAIL,
+                    clientId: process.env.CLIENT_ID,
+                    clientSecret: process.env.CLIENT_SECRET,
+                    refreshToken: process.env.REFRESH_TOKEN,
+                    accessToken: accessToken
+                }
+            })
+            const emailForm = args.emailForm
+            const mailOptions = {
+                from: process.env.EMAIL,
+                to: 'ahjaperearst@gmail.com',
+                subject: emailForm.subject,
+                replyTo: emailForm.replyTo,
+                text: emailForm.text,
+                html: emailForm.text
             }
-        })
-        const emailForm = args.emailForm
-        let mailOptions = {
-            from: process.env.EMAIL,
-            to: 'ahjaperearst@gmail.com',
-            subject: emailForm.subject,
-            replyTo: emailForm.replyTo,
-            text: emailForm.text,
-            html: emailForm.text
-        }
-        transporter.sendMail(mailOptions, (err, data) => {
-            if (err) {
-                throw new err
-            } else {
-                console.log("email sent")
+            await transporter.sendMail(mailOptions);
+            return {
+                response: "Email sent!"
             }
-        })
-        return {
-            response: "Email sent"
+        } catch (err) {
+            console.log(err)
         }
     }
 }
